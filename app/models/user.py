73530c73c96ac26597b65920337326510b4408dd
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2023-02-02 09:27:08 -0800

    Edited post comments

--- app/models/user.py
+++ app/models/user.py
@@ -83,7 +83,9 @@ class User(db.Model, UserMixin):
         '''
         return {
             'id': self.id,
-            'username': self.username,
+            # 'username': self.username,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
             'email': self.email,
             "profile_pic": self.profile_pic,
             'birthday': self.birthday,
