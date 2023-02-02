e11f1d58fc3b4242adcbc22f057adfe9b06ce474
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2023-02-01 19:50:09 -0800

    Created seeders for comments

---
+++ app/seeds/comments.py
@@ -0,0 +1,57 @@
+from app.models import db, environment, SCHEMA, Comment
+
+# Adds a demo user, you can add other users here if you want
+def seed_comments():
+    demo = Comment(
+        user_id = 1,
+        post_id = 2,
+        comment_text = 'There is a certain beauty in setting the world on fire and watching from the center of the flames'
+        )
+    mikael = Comment(
+        user_id = 2,
+        post_id = 3,
+        comment_text = 'Yeah I hate it!'
+        )
+    mikael2 = Comment(
+        user_id = 2,
+        post_text = 4,
+        post_text = 'Warzone 2???'
+        )
+    lily = Comment(
+        user_id = 4,
+        post_id = 1,
+        post_text = 'See ya'
+        )
+    sergio = Comment(
+        user_id = 5,
+        post_text = 4,
+        post_text = 'Hoppin on rn'
+        )
+    jennie = Comment(
+        user_id = 6,
+        post_id = 2,
+        post_text = 'I love when youre poetic'
+        )
+
+    db.session.add(demo)
+    db.session.add(mikael)
+    db.session.add(mikael2)
+    db.session.add(lily)
+    db.session.add(sergio)
+    db.session.add(jennie)
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_comments():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.comments RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute("DELETE FROM comments")
+
+    db.session.commit()
