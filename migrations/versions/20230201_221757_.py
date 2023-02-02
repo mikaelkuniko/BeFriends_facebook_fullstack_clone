aae92070e436f105def79b55061b616f5dbd360d
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2023-02-01 22:18:41 -0800

    Fixed seeders for comments"

--- migrations/versions/20230201_221757_.py
+++ migrations/versions/20230201_221757_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 09d43e28c9d1
+Revision ID: 0be6666c1878
 Revises: 
-Create Date: 2023-02-01 20:17:00.526715
+Create Date: 2023-02-01 22:17:57.413168
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '09d43e28c9d1'
+revision = '0be6666c1878'
 down_revision = None
 branch_labels = None
 depends_on = None
