3ec5250936937bb40faacd4be458d77ba07b7d8a
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2023-02-02 09:31:59 -0800

    Fixed sizing for create post modal

--- react-app/src/components/homepage/CreatePostForm.js
+++ react-app/src/components/homepage/CreatePostForm.js
@@ -4,7 +4,7 @@ import { Redirect, useHistory } from "react-router-dom";
 import * as postActions from '../../store/post'
 import './PostFormModal.css';
 
-function CreatePostForm({post}) {
+function CreatePostForm({post, onModalClose}) {
     const dispatch = useDispatch();
     const user = useSelector(state => state.session.user);
     const userId = user.id;
@@ -21,6 +21,7 @@ function CreatePostForm({post}) {
 
         return dispatch(postActions.postCreate(payload))
             // .then(()=>history.push('/homepage'));
+            .then(onModalClose())
 
     };
 
@@ -35,6 +36,9 @@ function CreatePostForm({post}) {
         placeholder={`What's on your mind, ${user.first_name}?`}
         id="text-area"
         value={content}
+        rows={8}
+        cols={64}
+        className='fixed-form-input'
         onChange={e => setContent(e.target.value)} />
         <button type="submit" id="post-button">Post</button>
         </form>
