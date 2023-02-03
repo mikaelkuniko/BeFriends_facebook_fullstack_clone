d5c3178d2f8a71ab9a6fa292c808dfac7303f884
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2023-02-03 01:30:05 -0800

    Completed splash page styling and added email validation to sig-up

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import './SignUpForm.css'
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
@@ -60,70 +61,82 @@ const SignUpForm = () => {
   }
 
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        <input
-          type='text'
-          onChange={(e)=> setFirstName(e.target.value)}
-          value={firstName}
-          placeholder='First Name'
-          required
-        ></input>
-        <input
-          type='text'
-          onChange={(e)=> setLastName(e.target.value)}
-          value={lastName}
-          placeholder='Last Name'
-          required
-        ></input>
+    <div className='sign-up-form-div'>
+      <div className='signup-text'>
+        <h1 id='title'>Sign Up</h1>
+        <h4 id='signup-subtext'>It's quick and easy.</h4>
       </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <p>Gender</p>
-          <div id="gender" onChange={(e) => setGender(e.target.value)}>
-            <label className="genders">Female
-              <input type="radio" id="female" name="gender" value="female"/>
-            </label>
-            <label className="genders">Male
-              <input type="radio" id="male" name="gender" value="male"/>
-            </label>
-            <label className="genders">Other
-              <input type="radio" id="other" name="gender" value="other"/>
-            </label>
-          </div>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+      <form onSubmit={onSignUp} className='sign-up-form'>
+        <div className='names'>
+          <input
+            type='text'
+            onChange={(e) => setFirstName(e.target.value)}
+            value={firstName}
+            placeholder='First Name'
+            required
+            className='fields'
+          >
+          </input>
+          <input
+            type='text'
+            onChange={(e) => setLastName(e.target.value)}
+            value={lastName}
+            placeholder='Last Name'
+            required
+            className='fields'
+          ></input>
+        </div>
+        <div>
+          <input
+            type='email'
+            name='email'
+            onChange={updateEmail}
+            placeholder='Email'
+            value={email}
+            className='fields'
+          ></input>
+        </div>
+        <div>
+          <input
+            type='password'
+            name='password'
+            onChange={updatePassword}
+            placeholder='Password'
+            value={password}
+            className='fields'
+          ></input>
+        </div>
+        <div>
+          <input
+            type='password'
+            name='repeat_password'
+            onChange={updateRepeatPassword}
+            value={repeatPassword}
+            required={true}
+            placeholder='Confirm Password'
+            className='fields'
+          ></input>
+        </div>
+        <p className='subtitle'>Gender</p>
+        <div className="gender-radio" onChange={(e) => setGender(e.target.value)}>
+          <label className="genders">Female
+            <input type="radio" id="female" name="gender" value="female" />
+          </label>
+          <label className="genders">Male
+            <input type="radio" id="male" name="gender" value="male" />
+          </label>
+          <label className="genders">Other
+            <input type="radio" id="other" name="gender" value="other" />
+          </label>
+        </div>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <button id='sign-up-button' type='submit'>Sign Up</button>
+      </form>
+    </div>
   );
 };
 
