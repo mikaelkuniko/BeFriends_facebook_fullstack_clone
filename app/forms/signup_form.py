2f2fd36ec919a087ea4167eec8572daafb28dd5a
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2023-02-03 08:29:02 -0800

    Updated validators for sign-up

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -23,7 +23,7 @@ def username_exists(form, field):
 class SignUpForm(FlaskForm):
     username = StringField('username')
     #     'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists, Email()])
+    email = StringField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired()])
     first_name = StringField('first name', validators=[DataRequired()])
     last_name = StringField('last name', validators=[DataRequired()])
