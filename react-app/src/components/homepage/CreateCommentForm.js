a3d5054e12658e1673a2b720d6bd1ced4c7f79f7
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2023-02-03 16:16:14 -0800

    Styled comments and added error handling

--- react-app/src/components/homepage/CreateCommentForm.js
+++ react-app/src/components/homepage/CreateCommentForm.js
@@ -9,8 +9,20 @@ function CreateCommentForm({ post }) {
     const user = useSelector(state => state.session.user);
     // const userId = user.id;
     const [content, setContent] = useState("");
+    const [errors, setErrors] = useState([])
     // const history = useHistory()
     // console.log('This is current post', post)
+
+    useEffect(() => {
+        const errors = []
+        if (content.length === 0) errors.push("Post must contain at least one character.")
+
+        setErrors(errors)
+    }, [content])
+
+    useEffect(() => {
+
+    }, [errors])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -43,7 +55,7 @@ function CreateCommentForm({ post }) {
                         onChange={e => setContent(e.target.value)}
                         className='input'
                     />
-                    <button type="submit" id="comment-button"><i class="fa-regular fa-comment" id='comment'></i></button>
+                    <button type="submit" id="comment-button" disabled={!!errors.length}><i class="fa-regular fa-comment" id='comment'></i></button>
                 </form>
             </div>
         </div>
--- react-app/src/components/homepage/EditCommentForm.js
+++ react-app/src/components/homepage/EditCommentForm.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect, useHistory, useParams } from "react-router-dom";
 import * as commentActions from '../../store/comment'
+import './EditCommentForm.css'
 
 function EditCommentForm({comment, onModalClose}) {
     const dispatch = useDispatch();
@@ -9,11 +10,23 @@ function EditCommentForm({comment, onModalClose}) {
 
     const userId = user.id;
     const [content, setContent] = useState(comment.comment.comment_text);
+    const [errors, setErrors] = useState([])
     const history = useHistory()
 
-    console.log('This is comment', comment)
+    // console.log('This is comment', comment)
 
     const commentId = comment.comment.id
+
+    useEffect(() => {
+        const errors = []
+        if (content.length === 0) errors.push("Post must contain at least one character.")
+
+        setErrors(errors)
+    }, [content])
+
+    useEffect(() => {
+
+    }, [errors])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -30,21 +43,38 @@ function EditCommentForm({comment, onModalClose}) {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="edit_post">
-        <div id="user-card">
-            {user && <p>profile img</p>}
-            <h4>{user && `${user.first_name} ${user.last_name}`}</h4>
-        </div>
+        <form onSubmit={handleSubmit} className="edit_comment">
+          <h2 id='edit-comment-header'>Edit Comment</h2>
+         <div id="user-card">
+                {user.profile_pic && <p>profile img</p>}
+                {!user.profile_pic && (
+                <div>
+                    <i class="fa-regular fa-user"></i>
+                    </div>)}
+                <div id='name'>
+                    <h4>{user && `${user.first_name} ${user.last_name}`}</h4>
+                </div>
+            </div>
         <textarea
         name="new-post"
-        rows={8}
-        cols={64}
+        rows={7}
+        cols={55}
         className='fixed-form-input'
         placeholder='Edit your comment...'
         id="text-area"
         value={content}
         onChange={e => setContent(e.target.value)} />
-        <button type="submit" id="edit-button">Edit</button>
+        {!!errors.length && (
+                <div className="create-post-errors">
+                    The following errors were found:
+                    <ul className='errors'>
+                        {errors.map((error) => (
+                            <li key={error}>{error}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        <button type="submit" id="edit-button" disabled={!!errors.length}>Edit</button>
         </form>
     );
 }
