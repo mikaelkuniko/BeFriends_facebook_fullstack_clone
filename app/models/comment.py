a42fe20d50c908cc218641c56a8144b1e7819b32
mikaelkuniko <mikaelkuniko@gmail.com>
2023-04-25 15:11:15 -0700

    Created models for likes for users for comments and posts

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,6 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
+from join_tables import comment_likes
 
 class Comment(db.Model):
     __tablename__ = 'comments'
@@ -15,9 +16,17 @@ class Comment(db.Model):
     created_at = db.Column(db.DateTime(timezone=True), default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), default=func.now())
 
+    # One to many relationship
+
     user = db.relationship('User', back_populates='comment')
 
     post = db.relationship('Post', back_populates='comment')
+
+    #  Many to many relationships
+
+    comment_user_likes = db.relationship("User",
+                                secondary=comment_likes,
+                                back_populates='user_comment_likes')
 
     def to_dict(self):
         """
