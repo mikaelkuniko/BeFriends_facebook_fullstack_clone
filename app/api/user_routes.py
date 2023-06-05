4cdebaebcd190e3b5c1d41b19026c80629b8ee02
mikaelkuniko <mikaelkuniko@gmail.com>
2023-06-05 08:51:44 -0700

    Updated user routes for likes

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
-from app.models import User
+from flask_login import login_required, current_user
+from app.models import User, db
 
 user_routes = Blueprint('users', __name__)
 
@@ -23,3 +23,43 @@ def user(id):
     """
     user = User.query.get(id)
     return user.to_dict()
+
+@user_routes.route('/<int:id>/postlike', methods=['DELETE'])
+@login_required
+def delete_post_like(id):
+    """
+    Delete the like that the User input on the post
+    """
+    current = current_user.to_dict()
+    user = User.query.get(current['id'])
+
+    if len(user.user_post_likes):
+        for i in range(len(user.user_post_likes)):
+            if user.user_post_likes[i].id == id:
+                user.user_post_likes.pop(i)
+                db.session.add(user)
+                db.session.commit()
+
+                return {'message': 'deleted like from post'}
+            
+    return {'errors': 'Post not found'}, 404
+
+@user_routes.route('/<int:id>/commentlike', methods=['DELETE'])
+@login_required
+def delete_post_like(id):
+    """
+    Delete the like that the User input on the comment
+    """
+    current = current_user.to_dict()
+    user = User.query.get(current['id'])
+
+    if len(user.user_post_likes):
+        for i in range(len(user.user_post_likes)):
+            if user.user_comment_likes[i].id == id:
+                user.user_comment_likes.pop(i)
+                db.session.add(user)
+                db.session.commit()
+
+                return {'message': 'deleted like from comment'}
+            
+    return {'errors': 'Comment not found'}, 404
