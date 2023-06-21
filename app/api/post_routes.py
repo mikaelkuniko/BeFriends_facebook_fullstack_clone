3cde36f9f075e4ef0e194292aea2b055964fa3a7
mikaelkuniko <mikaelkuniko@gmail.com>
2023-06-20 23:11:27 -0700

    Debugging likes

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -97,8 +97,8 @@ def add_post_like(id):
     user = User.query.get(current['id'])
     post = Post.query.get(id)
     # print('Inside the post like')
-    # print("This is the current user", user)
-    # print("This is the post ID", post)
+    print("This is the current user", user)
+    print("This is the post ID", post)
 
     post.post_user_likes.append(user)
     db.session.add(user)
