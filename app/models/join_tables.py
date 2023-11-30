bbaccb7f4d3d1e6e4739fb488af390d34f67cf7f
mikaelkuniko <mikaelkuniko@gmail.com>
2023-11-30 08:42:01 -0800

    Revising join tables

--- app/models/join_tables.py
+++ app/models/join_tables.py
@@ -8,6 +8,16 @@ post_likes = db.Table(
     db.Column("post_id", db.ForeignKey(
         add_prefix_for_prod('posts.id')), primary_key=True),
     db.Column("created_at", db.DateTime(timezone=True), default=func.now())
+
+    # create a method to query through created join tables
+# def to_dict_post_likes(self):
+# '''
+# Returns a dict representing the post id and user ids that liked
+# return {
+# self.user_id,
+# self.post_id
+# }
+# '''
 )
 
 comment_likes = db.Table(
@@ -17,7 +27,19 @@ comment_likes = db.Table(
     db.Column("comment_id", db.ForeignKey(
         add_prefix_for_prod('comments.id')), primary_key=True),
     db.Column("created_at", db.DateTime(timezone=True), default=func.now())
+
+    # def to_dict_comment_likes(self):
+# '''
+# Returns a dict representing the post id and user ids that liked
+# return {
+# self.user_id,
+# self.comment_id
+# }
+# '''
 )
+
+
+
 
 # create a method to query through created join tables
 # def to_dict_post_likes(self):
